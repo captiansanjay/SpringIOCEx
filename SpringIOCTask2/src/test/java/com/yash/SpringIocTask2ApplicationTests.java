f1ec7e21d67e9e612d60e8e03dfc12a8c1325af8
sanjay magadum <44236133+captiansanjay@users.noreply.github.com>
2022-09-15 10:20:29 +0530

    IOC task2

---
+++ SpringIOCTask2/src/test/java/com/yash/SpringIocTask2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIocTask2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
